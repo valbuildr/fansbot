3d21c4b1590aad9c1dc75d7eab300c71a0ee14af
valentine <v4lbuild3r@gmail.com>
2025-02-27 03:10:24 -0600

    small config changes

--- src/config.ex.py
+++ src/config.ex.py
@@ -1,18 +1,23 @@
-discord_token = ""
+DISCORD_TOKEN = ""
+SERVER_ID = 0
+MOD_ROLE_ID = 0
+HELPER_ROLE_ID = 0
+UNVERIFIED_ROLE_ID = 0
+MEMBER_ROLE_ID = 0
+RULES_CHANNEL_ID = 0
+TICKET_CATEGORY_ID = 0
+POLLS_CHANNEL_ID = 0
+SOURCE_CODE_LINK = ""
 
-server_id = 0
+# these are aliases
 
-mod_role_id = 0
-helper_role_id = 0
-
-unverified_role_id = 0
-member_role_id = 0
-
-rules_channel_id = 0
-rules_message_id = 0
-
-ticket_category_id = 0
-
-polls_channel_id = 0
-
-source_code_link = ""
+discord_token = DISCORD_TOKEN
+server_id = SERVER_ID
+mod_role_id = MOD_ROLE_ID
+helper_role_id = HELPER_ROLE_ID
+unverified_role_id = UNVERIFIED_ROLE_ID
+member_role_id = MEMBER_ROLE_ID
+rules_channel_id = RULES_CHANNEL_ID
+ticket_category_id = TICKET_CATEGORY_ID
+polls_channel_id = POLLS_CHANNEL_ID
+source_code_link = SOURCE_CODE_LINK
