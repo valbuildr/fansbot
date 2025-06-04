d76db1582ca6a42a20ad3439122332584bfabd3f
Valentine <v4lbuild3r@gmail.com>
2025-06-04 14:48:25 -0500

    :idiot:

--- src/main.py
+++ src/main.py
@@ -291,7 +291,7 @@ async def update_scheules():
                 ui.Section(
                     ui.TextDisplay("Full schedule:"),
                     accessory=ui.Button(
-                        url=radio_services[service["service_id"]]["bbc"], label="Open"
+                        url=radio_services[service['service_id']]['bbc'], label="Open"
                     ),
                 )
             )
