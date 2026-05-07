8cb2590640307bc765c12e74dffacfe5b1ada46d
Valentine <150450259+valbuildr@users.noreply.github.com>
2026-05-07 04:20:33 -0500

    Update snowflake epoch

--- src/utils/snowflake.ts
+++ src/utils/snowflake.ts
@@ -1,3 +1,5 @@
 import { SnowflakeIdGenerator } from "@codylabs/snowflake-id-generator";
 
-export default new SnowflakeIdGenerator();
+export default new SnowflakeIdGenerator(1n, 1n, {
+    epoch: 879096600000n // 9 November 1997 at 17:30 GMT
+});
